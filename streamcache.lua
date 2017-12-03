6980789db34cacfd29bffca7bf4b6a3a1abd75f9
Lutz Vieweg <lvml@5t9.de>
2017-12-03 20:05:36 +0100

    streamcache.lua has been modified a lot to reflect the much different caching behaviour of more contemporary mpv versions. A lua script has no more possibility to obtain the actual amount of data cached by mpv - but on the other hand, mpv does do a good job of maintaining a cache of "n seconds", using the "cache-secs" option. Surprisingly, mpv will report only the amount of data cached in excess of that many "cache-secs", when mp.get_property_native("cache-used") is called.
    So now, streamcachel.lua operates like this:
    
    - pass the number of seconds of cache to aim for to "cache-secs"
    
    - set the initial playback speed to "streamcache_min_speed" (default: 0.98)
    
    - slowly (meaning: by no more than the factor of "streamcache_adjust_factor" per second) decrease the playback speed only if "cache-used" reports zero kB available in addition to "cache-secs" seconds of cache
    
    - slowly increase the playback speed only if "cache-used" reports more than "streamcache_slack/3" seconds worth of kB reported by "cache-used" available in addition to "cache-secs" seconds of cache

--- streamcache.lua
+++ streamcache.lua
@@ -1,26 +1,30 @@
 -- streamcache.lua
 
--- Configurable parameters:
+-- This version of streamcache.lua has been modified a lot to reflect
+-- the much different caching behaviour of more contemporary mpv versions.
+-- A lua script has no more possibility to obtain the actual amount of
+-- data cached by mpv - but on the other hand, mpv does do a good job
+-- of maintaining a cache of "n seconds", using the "cache-secs" option.
+-- Surprisingly, mpv will report only the amount of data cached in excess
+-- of that many "cache-secs", when mp.get_property_native("cache-used")
+-- is called.
 --
--- speed will slowly be decreased towards streamcache_min_speed
---  as long as there is less data cached than required to bridge
---  "streamcache_cache_seconds_low" seconds of stalled transmission.
---
--- speed will slowly be increased towards 1.0 while there is more data
---  cached than required to bridge "streamcache_cache_seconds_high"
---  seconds of stalled transmission.
---
--- To keep replay speed adjustments below your attention threshold,
---  the replay speed factor will never be set below "streamcache_min_speed",
---  and this script will not change this factor by more than a factor of 
---  "streamcache_adjust_factor" per second.
+-- So now, streamcachel.lua operates like this:
+--  - pass the number of seconds of cache to aim for to "cache-secs"
+--  - set the initial playback speed to "streamcache_min_speed" (default: 0.98)
+--  - slowly (meaning: by no more than the factor of "streamcache_adjust_factor" per second)
+--    decrease the playback speed only if "cache-used" reports zero kB available
+--    in addition to "cache-secs" seconds of cache
+--  - slowly increase the playback speed only if "cache-used" reports more
+--    than "streamcache_slack/3" seconds worth of kB reported by "cache-used" available
+--    in addition to "cache-secs" seconds of cache
 
-streamcache_cache_seconds_low  = 20
-streamcache_cache_seconds_high = 30
+-- Configurable parameters:
+streamcache_seconds_aim = 30
+streamcache_slack = 10
 streamcache_min_speed = 0.98
 streamcache_adjust_factor = 1.0005
 
-streamcache_verbose = false
 
 -- Changing replay speed by < 2% seems to cause less
 --  distortion when not correcting audio pitch (via the scaletempo filter).
@@ -36,16 +40,14 @@ mp.set_property("options/audio-pitch-correction", "no")
 
 -- Anything below this line is not meant for configuration.
 
-streamcache_cache_low  = 500
+mp.set_property("options/cache-secs", streamcache_seconds_aim)
+
 streamcache_cache_high = 750
+streamcache_cache_low = 250
+streamcache_last_kb_per_sec = 1
 
-function streamcache_log(level, msg)
-	if (streamcache_verbose) then
-		mp.msg.log(level, msg)
-	end
-end
+function streamcache_compute_cache_sizes()	
 
-function streamcache_compute_cache_sizes()
 	local vb = mp.get_property_native("video-bitrate")
 	if (vb == nil) then
 		vb = 0
@@ -64,32 +66,20 @@ function streamcache_compute_cache_sizes()
 	end
 
 	-- compute the cache size required per second in kb
-	local kb_per_sec = br / (8 * 1024)	
+	local kb_per_sec = math.ceil(br / (8 * 1024))
+	
+	mp.msg.log("debug", "ab=" .. ab .. " vb=" .. vb .. " br=" .. br .. " kb_per_sec=" .. kb_per_sec .. " streamcache_last_kb_per_sec=" .. streamcache_last_kb_per_sec)
 
-	streamcache_log("info", "ab=" .. ab .. " vb=" .. vb .. " br=" .. br .. " kb_per_sec=" .. kb_per_sec)
-	
-	streamcache_cache_low  = streamcache_cache_seconds_low * kb_per_sec
-	streamcache_cache_high = streamcache_cache_seconds_high * kb_per_sec
-	
-	if (streamcache_verbose or ab > 0 or vb > 0) then
-		mp.msg.log("info", "assuming bitrate=" .. br/1024 .. "kbit/s, thresholds: cache_low=" .. streamcache_cache_low .. "kb cache_high=" .. streamcache_cache_high .. "kb")
-	end
-	
-	if (ab > 0 or vb > 0) then
-		-- possibly increase cache-size property only if at least some
-		-- information on audio or video bitrate exists
+	local rate_change = kb_per_sec / streamcache_last_kb_per_sec
+
+	if ((rate_change < 0.9) or (rate_change > 1.1)) then
+		streamcache_cache_high = streamcache_slack * kb_per_sec
+		streamcache_cache_low  = streamcache_cache_high / 3 
 		
-		local new_cache_size = streamcache_cache_high * 2
-		local cs = mp.get_property_native("cache-size")
-		if (cs == nil) then
-			cs = 0
-		end
-		if (new_cache_size > cs) then
-			-- we do not shrink the cache, only expand it when necessary
-			streamcache_log("info", "increased the cache-size to " .. new_cache_size .. "kb")
-			mp.set_property("cache-size", new_cache_size)
-		end
-	end
+		mp.msg.log("v", "bitrate changed from " .. streamcache_last_kb_per_sec .. " kB/s to " .. kb_per_sec .. " kB/s, streamcache_cache_high now " .. streamcache_cache_high .. " kB") 
+		
+		streamcache_last_kb_per_sec = kb_per_sec 
+	end		
 end
 mp.observe_property("audio-bitrate", "native", streamcache_compute_cache_sizes)
 mp.observe_property("video-bitrate", "native", streamcache_compute_cache_sizes)
@@ -104,43 +94,51 @@ function streamcache_check_fill()
 	if current_speed == nil then
  		current_speed = streamcache_min_speed
 	end
-	
-	if (cache_used < streamcache_cache_low) then
-		-- not enough in cache - so slowly deccelerate
-		local new_speed = current_speed * 0.9995
+
+	if (cache_used <= 0 or (cache_used < (streamcache_cache_high-streamcache_cache_low) and current_speed > 1.0)) then
+		-- slow down, but now below streamcache_min_speed
+		local new_speed = current_speed * (1.0 / streamcache_adjust_factor)
 		if (new_speed < streamcache_min_speed) then
 			new_speed = streamcache_min_speed
 		end
-		streamcache_log("info", "cache_used=" .. cache_used .. " (< low), new_speed=" .. new_speed)
-		mp.set_property("speed", new_speed)	
-		return
-	end
-	
-	if (cache_used > streamcache_cache_high) then
-		if (current_speed >= 1.0) then
-			-- all fine, nothing to do
-			streamcache_log("info", "cache_used=" .. cache_used .. " (> high) current_speed >= 1.0 - do nothing")	
-			return
-		end
-		
-		-- current speed is < 1.0, but there's really enough in the cache, so slowly accelerate
-		local new_speed = current_speed * 1.0005
-		if (1.0 - new_speed < 0.0001) then
+		if ((new_speed > (1.0/streamcache_adjust_factor)) and (new_speed < streamcache_adjust_factor)) then
+			-- new_speed is very near 1.0 - so let's use 1.0
 			new_speed = 1.0
 		end
-		streamcache_log("info", "cache_used=" .. cache_used .. " (> high), new_speed=" .. new_speed)
-		mp.set_property("speed", new_speed)	
+		if (new_speed ~= current_speed) then
+			mp.msg.log("info", "cache-used " .. cache_used .. " kB, lowered speed to " .. new_speed)
+			mp.set_property("speed", new_speed)	
+		end
+		return
+	end
+
+	if (cache_used > streamcache_cache_high or ((cache_used > streamcache_cache_low) and current_speed < 1.0)) then
+		-- speed up, but not above 1/streamcache_min_speed
+		local new_speed = current_speed * streamcache_adjust_factor
+		local max_speed = math.floor((10000.0/streamcache_min_speed) + 0.5)/10000.0
+		if (new_speed > max_speed) then
+			new_speed = max_speed
+		end
+		if ((new_speed > (1.0/streamcache_adjust_factor)) and (new_speed < streamcache_adjust_factor)) then
+			-- new_speed is very near 1.0 - so let's use 1.0
+			new_speed = 1.0
+		end
+		if (new_speed ~= current_speed) then
+			mp.msg.log("info", "cache-used " .. cache_used .. " kB, increased speed to " .. new_speed)
+			mp.set_property("speed", new_speed)	
+		end
 		return
 	end
 	
-	-- cache_used is between low and high - don't change speed
-	streamcache_log("info", "cache_used=" .. cache_used .. " (> low < high) no speed change")	
+	-- cache_used is ok, and currenty speed not higher or lower 1.0	
+	mp.msg.log("v", "cache-used " .. cache_used .. " kB, speed remains at " .. current_speed)	
 end
 
 streamcache_timer = mp.add_periodic_timer(1.0, streamcache_check_fill)
 
 
 function streamcache_on_loaded()
+	mp.msg.log("info", "new file loaded - starting with minimum speed = " .. streamcache_min_speed)
 	mp.set_property("speed", streamcache_min_speed)
 	streamcache_compute_cache_sizes()	
 end
